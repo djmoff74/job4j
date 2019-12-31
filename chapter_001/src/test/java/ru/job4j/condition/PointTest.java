04a5e33c7a46b4742e7e3d41b73900673b1ab902
djmoff74 <djmoff74@gmail.com>
2019-12-31 12:01:43 +0500

    123

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.condition;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.calculator.Fit;
@@ -7,12 +9,8 @@ import ru.job4j.calculator.Fit;
 public class PointTest {
     @Test
     public void distance() {
-        int x1 = 2;
-        int y1 = 11;
-        int x2 = 9;
-        int y2 = 14;
-        double expected = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
+        double result = Point.distance(2, 11, 9, 14);
+        double out = 7.615773105863909;
+        Assert.assertEquals(result, out, 0.01);
+            }
 }
