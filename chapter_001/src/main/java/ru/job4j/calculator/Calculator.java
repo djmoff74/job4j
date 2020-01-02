18da61bd8fedd1684bdb142256b154ff8e5bfd5c
djmoff74 <djmoff74@gmail.com>
2020-01-02 16:14:23 +0500

    5.8. JavaDoc. Code convention[#217062]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
 
+/**
+ * Class  Calculator для вычисления арифметических операций + - / *.
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version $Id$
+ * @since 02.01.2020
+ */
 public class Calculator {
     public static void add(double first, double second) {
         double result =  first + second;
@@ -17,6 +23,11 @@ public class Calculator {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
+
+    /**
+     * Main
+     * @param args - add div multiple subtract.
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
