b3d08d9ab06d01fa8c8599500f547352eb340340
djmoff74 <djmoff74@gmail.com>
2020-01-03 20:06:03 +0500

    6.7.1. Моно строка в матрице.[#217010]1231313

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,5 +1,13 @@
 package ru.job4j.array;
 
+/**
+ * Class MatrixCheck
+ *
+ * @author Lykov
+ * @version 1.0
+ * @since 03.01.2020
+ */
+
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
@@ -9,6 +17,17 @@ public class MatrixCheck {
                    break;
                 }
             }
+        return result;
+    }
+
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[row][cell] != 'X') {
+                result = false;
+                break;
+            }
+        }
         return result;
     }
 }
--- chapter_001/src/main/java/ru/job4j/calculator/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -2,7 +2,7 @@
  * Package for calculate task.
  *
  * @author Eduard Lykov (djmoff74@gmail.com)
- * @version $Id$
+ * @version 1.0
  * @since 0.1
  */
 package ru.job4j.calculator;
