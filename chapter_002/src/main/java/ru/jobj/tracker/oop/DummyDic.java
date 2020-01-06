7b919a92ee27f8201517ec417889b98efe211e38
djmoff74 <djmoff74@gmail.com>
2020-01-07 02:46:22 +0500

    1.5. Поля объекта[#217143]

--- chapter_002/src/main/java/ru/jobj/tracker/oop/DummyDic.java
+++ chapter_002/src/main/java/ru/jobj/tracker/oop/DummyDic.java
@@ -2,12 +2,7 @@ package ru.jobj.tracker.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        return eng;
-    }
-
-    public static void main(String[] args) {
-        DummyDic newWord = new DummyDic();
-        String s = newWord.engToRus("Hello");
-        System.out.println("Неизвестное слово. " + s);
+        String result = "Неизвестное слово. " + eng;
+        return result;
     }
 }
