b9b0ceaa3845132d884837bfeefada63f1b6bcb5
djmoff74 <djmoff74@gmail.com>
2020-01-07 19:26:57 +0500

    4. Расстояние между точками в трехмерном пространстве.[#217140]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -5,25 +5,62 @@ public class Point {
     /**
      * поле объекта. доступно только конкретному объекту
      */
-    private int x;
+    private final int x;
 
     /**
      * поле объекта. доступно только конкретному объекту
      */
-    private  int y;
+    private final int y;
 
+    /**
+     * поле объекта. доступно только конкретному объекту
+     */
+    private int z;
+
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка"
+     *
+     * @param first  координата x
+     * @param second координата y
+     */
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
-    public double distance(Point that) {
-                return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
-        }
 
-        public static void main(String[] args) {
-            Point a = new Point(2, 11);
-            Point b = new Point(9, 14);
-            double dist = a.distance(b);
-            System.out.println(dist);
-        }
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка"
+     *
+     * @param x координата x
+     * @param y координата y
+     * @param z координата z
+     */
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Метод distance подсчетывает расстояние между двумя точками
+     * @param that объект точки для вычисления
+     * @return double расстояние
+     */
+    public double distance(Point that) {
+       return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
+    }
+
+    /**
+     * Метод distance3d подсчетывает расстояние между двумя точками в трех мерном пространстве
+     * @param that объект точки для вычисления
+     * @return double расстояние
+     */
+    public double distance3d(Point that) {
+       return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2)
+                + Math.pow((this.z - that.z), 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -14,5 +14,14 @@ public class PointTest {
         double expected = 7.61;
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
-            }
+    }
+
+    @Test
+    public void distance3d() {
+        Point a = new Point(2, 11, 5);
+        Point b = new Point(9, 14, 10);
+        double expected = 9.11;
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
