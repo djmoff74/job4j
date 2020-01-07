d6d9dd940910e97fbb9f2339d0918d3da7a8cbc7
djmoff74 <djmoff74@gmail.com>
2020-01-07 22:26:28 +0500

    Item&Bug

--- chapter_002/src/main/java/ru/jobj/tracker/Item.java
+++ chapter_002/src/main/java/ru/jobj/tracker/Item.java
@@ -1,4 +1,12 @@
 package ru.jobj.tracker;
 
 public class Item {
+    public Item() {
+        super();
+        System.out.println("load item");
+    }
+
+    public static void main(String[] args) {
+        Bug bug = new Bug();
+    }
 }
