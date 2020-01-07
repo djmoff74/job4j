00f39f8abf4415e6b85f89281458bbc8ca305118
djmoff74 <djmoff74@gmail.com>
2020-01-07 16:34:45 +0500

    1.7. Состояние объекта.[#217148]

---
+++ chapter_002/src/main/java/ru/jobj/tracker/oop/Battery.java
@@ -0,0 +1,14 @@
+package ru.jobj.tracker.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+}
