c580bf6bfca97106aed1e6cda1a0d46005386c97
djmoff74 <djmoff74@gmail.com>
2020-01-07 22:58:45 +0500

    1. Конструктор по умолчанию.[#217153]

--- chapter_002/src/main/java/ru/jobj/oop/Animal.java
+++ chapter_002/src/main/java/ru/jobj/oop/Animal.java
@@ -1,15 +1,8 @@
 package ru.jobj.oop;
 
 public class Animal {
-    private String name;
     public Animal() {
         super();
-        System.out.println("class Animal");
-    }
-
-    public Animal(String name) {
-        super();
-        this.name = name;
         System.out.println("class Animal");
     }
 }
--- chapter_002/src/main/java/ru/jobj/oop/Tiger.java
+++ chapter_002/src/main/java/ru/jobj/oop/Tiger.java
@@ -2,13 +2,13 @@ package ru.jobj.oop;
 
 public class Tiger extends Predator {
     public Tiger() {
-        super();
-        System.out.println("class Tiger");
-    }
+            super();
+            System.out.println("class Tiger");
+        }
 
-    public static void main(String[] args) {
-        Animal animal = new Animal();
-        Predator predator = new Predator();
-        Tiger tiger = new Tiger();
-    }
+        public static void main(String[] args) {
+            Animal animal = new Animal();
+            Predator predator = new Predator();
+            Tiger tiger = new Tiger();
+        }
 }
