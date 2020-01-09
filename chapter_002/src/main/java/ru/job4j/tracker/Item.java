847f7dc2a303358030e468b266f3ad1a514de2ab
djmoff74 <djmoff74@gmail.com>
2020-01-10 02:33:02 +0500

    5. Реализовать класс Tracker[#217164]

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,12 +1,35 @@
 package ru.job4j.tracker;
 
+import java.util.Objects;
+
+/**
+ * Class Tracker
+ *
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version 1.0
+ * @since 09.01.2020
+ */
 public class Item {
-    public Item() {
-        super();
-        System.out.println("load item");
+    private String id;
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
     }
 
-    public static void main(String[] args) {
-        Bug bug = new Bug();
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Class TrackerTest
+ *
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version 1.0
+ * @since 10.01.2020
+ */
+public class TrackerTest {
+
+    @Test
+    public void checkAddItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        assertThat(tracker.findAll()[0], is(item));
+    }
+
+    @Test
+    public void findAllItems() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] result = tracker.findAll();
+        assertThat(result.length, is(3));
+    }
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItemName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item[] result = tracker.findByName(item.getName());
+        assertThat(result[0].getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+}
