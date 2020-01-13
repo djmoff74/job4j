a609cc13e0670d5dfa016a9d4e4dae3c5b55ed31
djmoff74 <djmoff74@gmail.com>
2020-01-13 10:55:14 +0500

    9. Написать тесты на StartUI.[#217129]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StubAction.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker;
+
+/**
+ * Class StubAction
+ *
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version 1.0
+ * @since 13.01.2020
+ */
+public class StubAction implements UserAction {
+    private boolean call = false;
+    @Override
+    public String name() {
+        return "Stub action";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        call = true;
+        return false;
+    }
+
+    public boolean isCall() {
+        return call;
+    }
+}
--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -47,4 +47,15 @@ public class StartUITest {
         deleteAction.execute(new StubInput(answers), tracker);
         assertThat(tracker.findById(answers[0]), is(nullValue()));
     }
+
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        assertThat(action.isCall(), is(true));
+    }
 }
+
