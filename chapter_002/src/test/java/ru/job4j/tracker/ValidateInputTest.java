9432808a38b169eb1ed6067145d783bd38205a74
djmoff74 <djmoff74@gmail.com>
2020-01-13 23:13:41 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#217133]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Class ValidateInputTest
+ *
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version 1.0
+ * @since 13.01.2020
+ */
+public class ValidateInputTest {
+    private final PrintStream def = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.def);
+        System.out.println("execute after method");
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"one", "1"})
+        );
+        input.askInt("Enter");
+        assertThat(
+                out.toString(),
+                is(String.format("Please enter validate data again.%n"))
+        );
+    }
+}
