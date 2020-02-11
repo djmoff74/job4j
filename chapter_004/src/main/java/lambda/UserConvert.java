94a9bf5e3e854682609b47833bfeb37522ef8d72
djmoff74 <djmoff74@gmail.com>
2020-02-11 18:26:14 +0500

    3. Ссылки на методы.[#216955]

---
+++ chapter_004/src/main/java/lambda/UserConvert.java
@@ -0,0 +1,45 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+/**
+ * Class UserConvert
+ *
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version 1.0
+ * @since 2/11/2020
+ */
+public class UserConvert {
+    public static class User {
+        private final String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "User{"
+                    + "name='" + name + '\''
+                    + '}';
+        }
+    }
+
+    public List<User> convert(List<String> names, Function<String, User> op) {
+        List<User> users = new ArrayList<>();
+        names.forEach(
+                n -> users.add(op.apply(n))
+        );
+        return users;
+    }
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Petr", "Nick", "Ban");
+        UserConvert users = new UserConvert();
+        List<User> data = users.convert(names, User::new);
+        data.forEach(System.out::println);
+
+    }
+}
